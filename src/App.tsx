3245efaf89c99118ee43c55b212b162611d6a1b8
VincentNdegwa <ndegwavincent7@gmail.com>
2024-06-21 21:35:32 +0300

    "Updated App.tsx, index.css, and sidebar components with changes to CSS classes and styles"

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ type Props = {};
 export default function App({}: Props) {
   return (
     <div className="flex h-full w-full gap-x-3">
-      <div className="w-2/6 text-sky-950 sticky top-0 left-0">
+      <div className="w-2/6 text-sky-950 sticky top-0 left-0 h-full">
         <div className="h-full">
           <SideBar />
         </div>
--- src/pages/sidebar/index.tsx
+++ src/pages/sidebar/index.tsx
@@ -4,11 +4,11 @@ type Props = {};
 
 function index({}: Props) {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="w-full rounded-md shadow-lg">
+    <div className="flex flex-col gap-4 w-full h-full">
+      <div className="w-full rounded-md shadow-lg h-16">
         <SearchBar />
       </div>
-      <div className="w-full mt-1 rounded-sm p-0">
+      <div className="w-full h-full mt-0 rounded-sm p-0 overflow-y-scroll scrollbar-none">
         <ContactList />
       </div>
     </div>
