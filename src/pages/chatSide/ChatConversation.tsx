7a2d0b31e552025d885e15372d75f5dd892d2087
VincentNdegwa <ndegwavincent7@gmail.com>
2024-06-21 10:23:12 +0300

    "Implemented ChatConversation component with UI elements and styling"

--- src/pages/chatSide/ChatConversation.tsx
+++ src/pages/chatSide/ChatConversation.tsx
@@ -1,8 +1,52 @@
+import { IoCheckmarkDoneOutline } from "react-icons/io5";
 
 function ChatConversation() {
   return (
-    <div>ChatConversation</div>
-  )
+    <div className="flex flex-col">
+      <div className="flex flex-col w-full gap-y-3">
+        <div className="left w-3/6 self-start">
+          <div className="flex gap-x-2">
+            <img
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt="profile-pic"
+              className="rounded-full h-10 w-10 gap-x-3 self-end"
+            />
+            <div className="text-sm">
+              <div className="p-2 bg-sky-700 text-white rounded-t-lg rounded-br-lg">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias eius nihil iusto consequatur aspernatur doloribus
+                porro corrupti exercitationem consectetur cupiditate deserunt in
+                aliquam, incidunt harum aut. Perferendis, accusamus tempora?
+                Quasi! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Animi, iste! Enim nostrum magni quae necessitatibus? Aliquid,
+                maxime tempore. Quae harum assumenda ipsa dolorum recusandae
+                reprehenderit molestias aliquid libero qui incidunt!
+              </div>
+              <div className="text-xs mt-2">5:12 PM</div>
+            </div>
+          </div>
+        </div>
+        <div className="right w-3/6 p-2 self-end">
+          <div className="text-sm flex flex-col">
+            <div className="p-2 bg-sky-100 text-sky-950 rounded-t-lg rounded-bl-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              eius nihil iusto consequatur aspernatur doloribus porro corrupti
+              exercitationem consectetur cupiditate deserunt in aliquam,
+              incidunt harum aut. Perferendis, accusamus tempora? Quasi! Lorem
+              ipsum dolor sit, amet consectetur adipisicing elit. Animi, iste!
+              Enim nostrum magni quae necessitatibus? Aliquid, maxime tempore.
+              Quae harum assumenda ipsa dolorum recusandae reprehenderit
+              molestias aliquid libero qui incidunt!
+            </div>
+            <div className="mt-2 self-end flex gap-x-1">
+              <div className="text-xs">5:12 PM</div>
+              <IoCheckmarkDoneOutline />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ChatConversation
+export default ChatConversation;
