ddbb8d5a771625292087e3146df87b522592735f
VincentNdegwa <ndegwavincent7@gmail.com>
2024-07-09 10:13:19 +0300

    "Added MdLogout icon, refactored SearchBar component with new imports, and updated event handling for menu toggle and click outside detection."

--- src/pages/sidebar/SearchBar.tsx
+++ src/pages/sidebar/SearchBar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useEffect, useRef } from "react";
 import { IoMdAddCircle } from "react-icons/io";
 import { IoNotificationsOutline, IoSettingsOutline } from "react-icons/io5";
@@ -5,6 +6,7 @@ import { FaUserCircle } from "react-icons/fa";
 import { CiMenuKebab } from "react-icons/ci";
 import { User } from "../../types";
 import useCustomAxios from "../../modules/customAxios";
+import { MdLogout } from "react-icons/md";
 
 type Props = {
   openProfile: () => void;
@@ -14,14 +16,21 @@ type Props = {
 export default function SearchBar({ openProfile, user }: Props) {
   const [optionMenu, setOptionMenu] = useState(false);
   const menuRef = useRef<HTMLDivElement | null>(null);
+  const buttonRef = useRef<HTMLDivElement | null>(null);
+
   const axios = useCustomAxios();
 
-  const toggleMenu = () => {
+  const toggleMenu = (ev: any) => {
+    ev.stopPropagation();
     setOptionMenu(!optionMenu);
   };
 
   const handleClickOutside = (event: MouseEvent) => {
-    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target as Node) &&
+      !buttonRef.current?.contains(event.target as Node)
+    ) {
       setOptionMenu(false);
     }
   };
@@ -61,27 +70,35 @@ export default function SearchBar({ openProfile, user }: Props) {
           </div>
         </div>
         <div className="flex items-center gap-4 text-white text-2xl relative">
-          <IoNotificationsOutline className="cursor-pointer rounded-full w-6 h-6 hover:bg-sky-900 p-1 hover:text-yellow-300" />
-          <IoSettingsOutline className="cursor-pointer rounded-full w-6 h-6 hover:bg-sky-900 p-1 hover:text-yellow-300" />
-          <IoMdAddCircle className="cursor-pointer rounded-full w-6 h-6 hover:bg-sky-900 p-1 hover:text-yellow-300" />
-          <CiMenuKebab
-            className="cursor-pointer rounded-full w-6 h-6 hover:bg-sky-900 p-1 hover:text-yellow-300"
-            onClick={toggleMenu}
-          />
+          <span>
+            <IoNotificationsOutline className="cursor-pointer rounded-full w-6 h-6 hover:bg-sky-900 p-1 hover:text-yellow-300" />
+          </span>
+          <span ref={buttonRef} onClick={(ev) => toggleMenu(ev)}>
+            <CiMenuKebab className="cursor-pointer rounded-full w-6 h-6 hover:bg-sky-900 p-1 hover:text-yellow-300" />
+          </span>
+
           {optionMenu && (
             <div
               className="absolute top-full right-0 mt-2 w-48 py-1 bg-sky-600 shadow-lg rounded-sm z-10"
               ref={menuRef}>
               <ul className="flex flex-col">
                 <li
-                  className="hover:bg-sky-700 p-2 text-[12px] cursor-pointer"
+                  className="hover:bg-sky-700 p-2 text-[12px] cursor-pointer flex items-center gap-x-3"
                   onClick={() => setOptionMenu(false)}>
-                  New chat
+                  <IoMdAddCircle />
+                  <div>New chat</div>
                 </li>
                 <li
-                  className="hover:bg-sky-700 p-2 text-[12px] cursor-pointer"
+                  className="hover:bg-sky-700 p-2 text-[12px] cursor-pointer flex items-center gap-x-3"
+                  onClick={() => setOptionMenu(false)}>
+                  <IoSettingsOutline />
+                  <div>Settings</div>
+                </li>
+                <li
+                  className="hover:bg-sky-700 p-2 text-[12px] cursor-pointer flex items-center gap-x-3"
                   onClick={() => logoutTheUser()}>
-                  Logout
+                  <MdLogout />
+                  <div>Logout</div>
                 </li>
               </ul>
             </div>
