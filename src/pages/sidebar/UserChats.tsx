092213478efb19895a016e509cb2eb5d6fe064fb
VincentNdegwa <ndegwavincent7@gmail.com>
2024-07-10 21:51:20 +0300

    "Added IoSearch icon, BlankLoading component, and loading state to UserChats component; refactored code structure and styling"

--- src/pages/sidebar/UserChats.tsx
+++ src/pages/sidebar/UserChats.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import { IoSearch } from "react-icons/io5";
 import useCustomAxios from "../../modules/customAxios";
 import { getUserId } from "../../modules/getUserId";
-import { IoSearch } from "react-icons/io5";
 import { User } from "../../types";
-
+import BlankLoading from "../Components/BlankLoading";
 type Props = {
   closeUserChat: () => void;
 };
@@ -13,17 +13,20 @@ function UserChats({ closeUserChat }: Props) {
   const axios = useCustomAxios();
   const [userId, setUserId] = useState<number | null>(null);
   const [users, setUser] = useState<User[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
   const closeNewChat = () => {
     closeUserChat();
   };
 
   useEffect(() => {
-    const uId = getUserId;
+    const uId = getUserId();
     setUserId(uId);
   }, []);
 
   useEffect(() => {
     if (userId) {
+      setLoading(true);
       axios
         .get(`/users/all/${userId}`)
         .then((res) => {
@@ -33,15 +36,24 @@ function UserChats({ closeUserChat }: Props) {
             setUser(res.data.data);
           }
         })
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
     }
   }, [axios, userId]);
+
   const startChat = (user: User) => {
     console.log(user);
   };
+  if (!users) {
+    return (
+      <div className="flex flex-col items-center justify-center h-full">
+        <div className="text-xl text-gray-600">No users found</div>
+      </div>
+    );
+  }
 
   return (
-    <div className="flex flex-col gap-y-0 items-center text-sky-950 w-full h-ful">
+    <div className="flex flex-col gap-y-0 items-center text-sky-950 w-full h-full">
       <div className="flex gap-x-3 items-center bg-sky-900 w-full h-[10vh] p-2 text-white">
         <div
           onClick={() => closeNewChat()}
@@ -61,31 +73,34 @@ function UserChats({ closeUserChat }: Props) {
             />
           </div>
         </div>
-        <ul role="list" className="divide-y divide-gray-100 mt-1">
-          {users &&
-            users.map((person) => (
-              <li
-                key={person.id}
-                onClick={() => startChat(person)}
-                className="flex justify-between gap-x-6 p-2 cursor-default hover:bg-gray-100 transition-all ease-out duration-700">
-                <div className="flex min-w-0 gap-x-4">
-                  <img
-                    alt=""
-                    src={person.profile?.profile_pic || "/images/avatar.png"}
-                    className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                  />
-                  <div className="min-w-0 flex-auto">
-                    <p className="text-sm font-semibold leading-6 text-gray-900">
-                      {person.profile?.first_name || person.phone_number}
-                    </p>
-                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                      {person.phone_number}
-                    </p>
+        {!loading && (
+          <ul role="list" className="divide-y divide-gray-100 mt-1">
+            {users &&
+              users.map((person) => (
+                <li
+                  key={person.id}
+                  onClick={() => startChat(person)}
+                  className="flex justify-between gap-x-6 p-2 cursor-default hover:bg-gray-100 transition-all ease-out duration-700">
+                  <div className="flex min-w-0 gap-x-4">
+                    <img
+                      alt=""
+                      src={person.profile?.profile_pic || "/images/avatar.png"}
+                      className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                    />
+                    <div className="min-w-0 flex-auto">
+                      <p className="text-sm font-semibold leading-6 text-gray-900">
+                        {person.profile?.first_name || person.phone_number}
+                      </p>
+                      <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                        {person.phone_number}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </li>
-            ))}
-        </ul>
+                </li>
+              ))}
+          </ul>
+        )}
+        {loading && <BlankLoading />}
       </div>
     </div>
   );
