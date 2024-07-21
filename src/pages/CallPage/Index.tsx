76f40a701dd18e780df7c48f54c069198f57da86
VincentNdegwa <ndegwavincent7@gmail.com>
2024-07-21 15:20:28 +0300

    "Refactored CallPage/Index.tsx: extracted setupIncomingConnection and setupOutgoingConnection functions, reorganized code, and updated video and button layouts."

--- src/pages/CallPage/Index.tsx
+++ src/pages/CallPage/Index.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from "react";
 import {
   FaMicrophone,
-  FaMicrophoneSlash,
+  // FaMicrophoneSlash,
   FaVideo,
   FaPhoneSlash,
 } from "react-icons/fa";
@@ -32,108 +32,107 @@ function Index({ mode, incommingCall }: Props) {
 
   useEffect(() => {
     if (incommingCall) {
-      const nPeer = new Peer();
-      nPeer.on("open", (id: string) => {
-        if (mode.sender_id != undefined) {
-          console.log("ansewering peer id: " + id);
-          socket.emit("answer-call", {
-            to: mode.sender_id?.toString(),
-            peerId: id,
-          });
-        }
-      });
-      nPeer.on("call", (call) => {
-        navigator.mediaDevices
-          .getUserMedia({ audio: true, video: mode.mode === callMode.VIDEO })
-          .then((stream) => {
-            call.answer(stream);
-            call.on("stream", (remoteStream) => {
-              if (remoteVideoRef.current) {
-                remoteVideoRef.current.srcObject = remoteStream;
-                console.log("call received");
-              }
-            });
-          });
-      });
-
-      console.log("start incomming call");
+      setupIncomingConnection();
     }
   }, [incommingCall]);
+
+  useEffect(() => {
+    if (peerId) {
+      handleCall(peerId);
+    }
+  }, [peerId]);
+
+  const setupOutgoingConnection = () => {
+    const newPeer = new Peer();
+    setPeer(newPeer);
+
+    newPeer.on("open", (id: string) => {
+      console.log(`My peer ID is: ${id}`);
+      window.navigator.mediaDevices
+        .getUserMedia({ audio: true, video: mode.mode === callMode.VIDEO })
+        .then((stream) => {
+          setLocalStream(stream);
+          if (localVideoRef.current) {
+            localVideoRef.current.srcObject = stream;
+          }
+          socket.emit("join", mode.sender_id);
+          socket.emit("call-user", mode);
+        })
+        .catch(console.error);
+    });
+
+    newPeer.on("error", console.error);
+  };
+
+  const setupIncomingConnection = () => {
+    const nPeer = new Peer();
+    setPeer(nPeer);
+
+    nPeer.on("open", (id: string) => {
+      socket.emit("answer-call", {
+        to: mode.sender_id,
+        peerId: id,
+      });
+    });
+
+    nPeer.on("call", (call) => {
+      window.navigator.mediaDevices
+        .getUserMedia({ audio: true, video: mode.mode === callMode.VIDEO })
+        .then((stream) => {
+          call.answer(stream);
+          call.on("stream", (remoteStream) => {
+            if (remoteVideoRef.current) {
+              remoteVideoRef.current.srcObject = remoteStream;
+            }
+          });
+        });
+    });
+  };
+
+  const handleCall = (peerId: string) => {
+    if (peer && localStream) {
+      const call = peer.call(peerId, localStream);
+      call.on("stream", (remoteStream) => {
+        if (remoteVideoRef.current) {
+          remoteVideoRef.current.srcObject = remoteStream;
+        }
+      });
+
+      call.on("close", () => {
+        if (remoteVideoRef.current) {
+          remoteVideoRef.current.srcObject = null;
+        }
+      });
+
+      call.on("error", console.error);
+    }
+  };
 
   socket.on("call-accepted", (data) => {
     setPeerId(data.peerId);
   });
 
-  useEffect(() => {
-    if (peerId != undefined) {
-      console.log("call answered " + peerId);
-      if (peer != null) {
-        console.log("found the peer");
-        if (localStream) {
-          const call = peer.call(peerId, localStream);
-          call.on("stream", (remoteStream: any) => {
-            if (remoteVideoRef.current) {
-              remoteVideoRef.current.srcObject = remoteStream;
-            }
-          });
-
-          call.on("close", () => {
-            if (remoteVideoRef.current) {
-              remoteVideoRef.current.srcObject = null;
-            }
-          });
-
-          call.on("error", (err) => {
-            console.error("PeerJS call error:", err);
-          });
-        }
-      }
-    }
-  }, [peerId]);
-  const setupOutgoingConnection = () => {
-    if (mode.sender_id && !incommingCall) {
-      const newPeer = new Peer();
-      setPeer(newPeer);
-
-      newPeer.on("open", (id: string) => {
-        console.log(`My peer ID is: ${id}`);
-      });
-
-      newPeer.on("error", (err: any) => {
-        console.error("PeerJS error:", err);
-      });
-
-      window.navigator.mediaDevices
-        .getUserMedia({ audio: true, video: mode.mode === callMode.VIDEO })
-        .then((stream) => {
-          if (localVideoRef.current) {
-            localVideoRef.current.srcObject = stream;
-            setLocalStream(stream);
-            socket.emit("join", mode.sender_id);
-            socket.emit("call-user", mode);
-          }
-        })
-        .catch((error) => {
-          console.error("Error setting up connection:", error);
-        });
-    }
-  };
-
   return (
-    <div className="absolute top-0 left-0 w-full h-full bg-sky-100 flex flex-col items-center justify-center p-4">
-      <div className="bg-white shadow-lg rounded-lg p-6 w-full flex flex-col items-center">
+    <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center p-1">
+      <div className=" bg-slate-900 w-4/6 h-full flex flex-col items-center relative">
         {mode.mode === callMode.VIDEO && (
-          // <div className="w-full h-64 bg-sky-900 rounded-lg mb-4 relative">
-          <div className="relative">
-            <video
-              ref={remoteVideoRef}
-              autoPlay
-              muted
-              className="w-full h-full rounded-lg"></video>
-            <video
-              ref={localVideoRef}
-              autoPlay
-              className=" w-40 h-40 border border-1 rounded-lg absolute top-0 left-0"></video>
+          <div className="bg-slate-900">
+            <div className="rounded-lg absolute top-0 left-0 w-40 h-40 z-20">
+              <video
+                ref={remoteVideoRef}
+                autoPlay
+                muted
+                disablePictureInPicture
+                className="object-cover w-full h-full"></video>
+            </div>
+            <div className="absolute top-0 right-0 w-full h-full rounded-2xl z-10">
+              <video
+                ref={localVideoRef}
+                autoPlay
+                muted
+                disablePictureInPicture
+                className="object-cover w-full h-full"></video>
+            </div>
           </div>
         )}
         {mode.mode === callMode.VOICE && (
@@ -141,29 +140,16 @@ function Index({ mode, incommingCall }: Props) {
             <FaMicrophone size={32} className="text-white" />
           </div>
         )}
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 absolute bottom-4 z-30">
           <button className="bg-red-600 text-white p-4 rounded-full shadow-lg hover:bg-red-500">
             <FaPhoneSlash size={24} />
           </button>
-          {mode.mode === callMode.VIDEO ? (
-            <>
-              <button className="bg-sky-700 text-white p-4 rounded-full shadow-lg hover:bg-sky-600">
-                <FaVideo size={24} />
-              </button>
-              <button className="bg-sky-700 text-white p-4 rounded-full shadow-lg hover:bg-sky-600">
-                <FaMicrophone size={24} />
-              </button>
-            </>
-          ) : (
-            <>
-              <button className="bg-sky-700 text-white p-4 rounded-full shadow-lg hover:bg-sky-600">
-                <FaMicrophone size={24} />
-              </button>
-              <button className="bg-sky-700 text-white p-4 rounded-full shadow-lg hover:bg-sky-600">
-                <FaMicrophoneSlash size={24} />
-              </button>
-            </>
-          )}
+          <button className="bg-sky-700 text-white p-4 rounded-full shadow-lg hover:bg-sky-600">
+            <FaVideo size={24} />
+          </button>
+          <button className="bg-sky-700 text-white p-4 rounded-full shadow-lg hover:bg-sky-600">
+            <FaMicrophone size={24} />
+          </button>
         </div>
       </div>
     </div>
