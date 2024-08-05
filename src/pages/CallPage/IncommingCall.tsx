0295db032f8bef8547d1bc1cac61dae1a2d630a6
VincentNdegwa <ndegwavincent7@gmail.com>
2024-08-05 23:23:07 +0300

    "Refactor IncommingCall component: updated imports, renamed `props` to `Props`, removed unnecessary code, and reorganized logic for handling local stream, peer connections, and call responses."

--- src/pages/CallPage/IncommingCall.tsx
+++ src/pages/CallPage/IncommingCall.tsx
@@ -1,43 +1,43 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { MdCallEnd } from "react-icons/md";
-import { Participant, callMode, callerData } from "../../types";
+import { MdCallEnd, MdOutlineCallEnd } from "react-icons/md";
 import { FaMicrophone, FaMicrophoneSlash, FaVideo } from "react-icons/fa";
-import { MdOutlineCallEnd } from "react-icons/md";
 import { useEffect, useRef, useState } from "react";
 import CustomSocket from "../../modules/CustomSocket";
 import Peer, { MediaConnection } from "peerjs";
 import CallerNotifier from "./CalllerNotifier";
 import VideoCallDisplays from "./VideoCallDisplays";
-type props = {
+import { Participant, callMode, callerData } from "../../types";
+
+type Props = {
   mode: callerData;
   incommingCall: boolean;
 };
 
 const socket = CustomSocket.getSocket();
 
-const IncommingCall = ({ mode, incommingCall }: props) => {
+const IncommingCall = ({ mode, incommingCall }: Props) => {
   const [muted, setMuted] = useState<boolean>(false);
   const [localStream, setLocalStream] = useState<MediaStream>();
   const localVideoRef = useRef<HTMLVideoElement>(null);
   const remoteVideoRef = useRef<HTMLVideoElement>(null);
-  const localStreamRef = useRef<MediaStream | null>(null);
   const [senderPeer, setSenderPeer] = useState<Peer>();
   const [receiverPeerId, setReceiverPeerId] = useState<string>();
   const [inCall, setInCall] = useState<MediaConnection>();
   const [remoteStreamIsSet, setRemoteStreamIsSet] = useState<boolean>(false);
   const [callAccepted, setCallAccepted] = useState<boolean>(false);
-  const callerUser: Participant | undefined = mode.sender;
   const [connecting, setConnecting] = useState<boolean>(false);
 
+  const callerUser: Participant | undefined = mode.sender;
+
   const toggleMute = () => {
-    if (localStreamRef.current) {
-      localStreamRef.current.getAudioTracks().forEach((track) => {
+    if (localStream) {
+      localStream.getAudioTracks().forEach((track) => {
         track.enabled = !track.enabled;
       });
-      setLocalStream(localStreamRef.current);
       setMuted(!muted);
     }
   };
+
   const handleCallResponse = (status: string) => {
     if (status === "answer") {
       setCallAccepted(true);
@@ -67,15 +67,14 @@ const IncommingCall = ({ mode, incommingCall }: props) => {
       });
       if (localVideoRef.current) {
         localVideoRef.current.srcObject = stream;
-        setConnecting(false);
-        setLocalStream(stream);
-        const peer = new Peer();
-        peer.on("open", (id: string) => {
-          console.log(`caller peerId ${id}`);
-        });
-        setSenderPeer(peer);
-        socket.emit("call-user", mode);
       }
+      setLocalStream(stream);
+      const peer = new Peer();
+      peer.on("open", (id: string) => {
+        console.log(`caller peerId ${id}`);
+      });
+      setSenderPeer(peer);
+      socket.emit("call-user", mode);
     } catch (err) {
       console.error("Error getting local stream:", err);
     }
@@ -86,32 +85,28 @@ const IncommingCall = ({ mode, incommingCall }: props) => {
     const peer = new Peer();
 
     peer.on("open", (id: string) => {
-      if (callerUser?.user.id != undefined) {
+      if (callerUser?.user.id !== undefined) {
         console.log("answering peer id: " + id);
         socket.emit("answer-call", {
-          to: callerUser?.user.id?.toString(),
+          to: callerUser.user.id.toString(),
           peerId: id,
         });
       }
     });
 
     peer.on("call", async (call) => {
-      const str = await getLocalStream();
-      setLocalStream(str);
-      localStreamRef.current = str;
+      const stream = await getLocalStream();
       if (localVideoRef.current) {
-        localVideoRef.current.srcObject = str;
-        setConnecting(false);
+        localVideoRef.current.srcObject = stream;
       }
-      console.log("got the local stream");
-
-      call.answer(localStreamRef.current);
+      call.answer(stream);
       setInCall(call);
 
-      call.on("stream", (remStream) => {
+      call.on("stream", (remoteStream) => {
         if (remoteVideoRef.current) {
-          remoteVideoRef.current.srcObject = remStream;
+          remoteVideoRef.current.srcObject = remoteStream;
           setRemoteStreamIsSet(true);
+          setConnecting(false);
           console.log("Setting up the remote stream");
         }
       });
@@ -134,11 +129,6 @@ const IncommingCall = ({ mode, incommingCall }: props) => {
         video: mode.mode === callMode.VIDEO,
       });
       setLocalStream(stream);
-      localStreamRef.current = stream;
-      if (localVideoRef.current) {
-        localVideoRef.current.srcObject = stream;
-        setConnecting(false);
-      }
       return stream;
     } catch (err) {
       console.error("Error getting media stream:", err);
@@ -146,40 +136,39 @@ const IncommingCall = ({ mode, incommingCall }: props) => {
     }
   };
 
+  useEffect(() => {
+    if (receiverPeerId && senderPeer && localStream) {
+      console.log("call answered " + receiverPeerId);
+      const call = senderPeer.call(receiverPeerId, localStream);
+      console.log(`calling receiverPeerId: ${receiverPeerId}`);
+
+      call.on("stream", (remoteStream: MediaStream) => {
+        if (remoteVideoRef.current) {
+          remoteVideoRef.current.srcObject = remoteStream;
+          setRemoteStreamIsSet(true);
+          setConnecting(false);
+          console.log("Received remote stream");
+        }
+      });
+
+      call.on("close", () => {
+        if (remoteVideoRef.current) {
+          remoteVideoRef.current.srcObject = null;
+        }
+      });
+
+      call.on("error", (err) => {
+        console.error("PeerJS call error:", err);
+      });
+
+      setInCall(call);
+    }
+  }, [localStream, receiverPeerId, senderPeer]);
+
   socket.on("call-accepted", (data) => {
     console.log("call accepted");
     setReceiverPeerId(data.peerId);
   });
-
-  useEffect(() => {
-    if (receiverPeerId) {
-      console.log("call answered " + receiverPeerId);
-      if (senderPeer && localStream) {
-        const call = senderPeer.call(receiverPeerId, localStream);
-        console.log(`calling receiverPeerId: ${receiverPeerId}`);
-        call.on("stream", (remoteStream: MediaStream) => {
-          if (remoteVideoRef.current) {
-            remoteVideoRef.current.srcObject = remoteStream;
-            setConnecting(false);
-            setRemoteStreamIsSet(true);
-            console.log("Received remote stream");
-          }
-        });
-
-        call.on("close", () => {
-          if (remoteVideoRef.current) {
-            remoteVideoRef.current.srcObject = null;
-          }
-        });
-
-        call.on("error", (err) => {
-          console.error("PeerJS call error:", err);
-        });
-      }
-    }
-  }, [localStream, receiverPeerId, senderPeer]);
-
-  
 
   return (
     <div className="w-full h-full bg-sky-950 relative">
@@ -189,7 +178,6 @@ const IncommingCall = ({ mode, incommingCall }: props) => {
             <div className="h-full flex justify-center">
               <CallerNotifier mode={mode} status="" />
             </div>
-
             <div className="flex space-x-3 md:space-x-5 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 bottom-4 z-30 bg-black bg-opacity-30 rounded-md p-1 items-center">
               <div
                 onClick={() => handleCallResponse("reject")}
